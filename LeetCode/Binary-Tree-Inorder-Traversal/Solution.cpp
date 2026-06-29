d91e0a349475b8b37f446125d0f430851c9e5a7c
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-06-29 23:55:33 +0530

    Implement inorder traversal for binary tree
    Added definition for TreeNode structure and implemented inorder traversal using stack.

--- LeetCode/Binary-Tree-Inorder-Traversal/Solution.cpp
+++ LeetCode/Binary-Tree-Inorder-Traversal/Solution.cpp
@@ -6,3 +6,37 @@
 [52,null,13,null,52,null,47,null,-75,-16,null,-10,null,7,99,10,null,20,null,15,-100,null,-37,67,0,-86,null,-96,25,null,68,-20,25,-28,-94,null,37,-22,null,65,-72,-24,null,90,19,null,86,-40,null,-7]
 [36,null,9,null,62,66,null,56,97,55,null,51,-21,null,81,62,-88,null,-67,-40,null,-61,null,-79,98,null,37,-42,-60,-61,null,-70,-47,null,67,90,22,27,null,88,-34,null,43,67,-10,null,73,null,42,null,-21,1,-46,-58,null,33,null,72,-20,null,82,-23,null,87,27,null,-34,62,60,null,-49,null,-74,-5,null,49,null,-85,36,5,60,null,71,59,57,null,-68,55,null,-36,11,-18,null,-87,-91,null,-74,null,-17,-25]
 [9,null,34,6,-17,null,-94,-54,60,-99,-30,null,100,null,-16,57,null,-90,-11,null,-17,null,-72,null,70,null,56,null,4,-65,null,-75,null,20,71,null,1,-7,-54,30,null,66,null,59,98,null,31,78,-7,-87,null,-4,25,96,-56,null,39,-63,null,-95,null,-37,80,null,-52,null,-85,31,null,51,null,3,82,null,-50,null,95,60,null,49,75,null,91,36,null,-27,null,-87,-13,null,96,null,87,null,19,null,-96,-69,null,-2]
+ 
+  
+  /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(!root){return ans;}
+        stack<TreeNode*>st;
+        while(root || !st.empty()){
+            if(root != nullptr){
+                st.push(root);
+                root = root->left;
+            }
+            else{
+                root = st.top();
+                st.pop();
+                if(root != nullptr) ans.push_back(root->val);
+                if(root != nullptr) root = root->right;
+            }
+        }
+        return ans;
+    }
+};
