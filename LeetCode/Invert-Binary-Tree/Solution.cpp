a9820b02badf62947fa19e1ad0e5d4ba16b3708c
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-07-01 00:14:59 +0530

    Add solution for Invert Binary Tree

--- LeetCode/Invert-Binary-Tree/Solution.cpp
+++ LeetCode/Invert-Binary-Tree/Solution.cpp
@@ -1,5 +1,8 @@
-       4
-     /   \
-    2     7
-   / \   / \
-  1   3 6   9
+TreeNode* invertTree(TreeNode* root) {
+    if (root) {
+        invertTree(root->left);
+        invertTree(root->right);
+        std::swap(root->left, root->right);
+    }
+    return root;
+}
