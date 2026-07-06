07cbc72abb2cdf6493d06d19d300b70ee0adbd90
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-07-06 22:24:42 +0530

    Add solution for Lowest Common Ancestor of a Binary Search Tree

--- LeetCode/Lowest-Common-Ancestor-of-a-Binary-Search-Tree/Solution.cpp
+++ LeetCode/Lowest-Common-Ancestor-of-a-Binary-Search-Tree/Solution.cpp
@@ -1,6 +1,29 @@
-[3,1,4,null,2]
-2
-3
-[5,3,6,2,4,null,null,1]
-1
-4
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+8 * };
+9 */
+10
+11class Solution {
+12    TreeNode*find_LCA(TreeNode*root, int p, int q){
+13        if(root == nullptr) {return root;}
+14
+15        if(p == root->val || q == root->val){return root;}
+16
+17        TreeNode*left =  find_LCA(root->left, p, q);
+18        TreeNode*right = find_LCA(root->right, p, q);
+19
+20        if(!left){return right;}
+21        else if(!right){return left;}
+22
+23        return root;
+24    }
+25public:
+26    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+27        return find_LCA(root, p->val, q->val);
+28    }
+29};
