0e0dee2a11116012785d4fdd60e164f1e2494424
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-07-13 23:13:13 +0530

    Add solution for Ugly Number

--- LeetCode/Ugly-Number/Solution.cpp
+++ LeetCode/Ugly-Number/Solution.cpp
@@ -1,13 +1,15 @@
-1class Solution {
-2public:
-3    bool isUgly(int n) {
-4        
-5        if(n == 0){return false;}
-6        while(n%2 == 0 || n%3 == 0|| n%5 == 0){
-7            if(n %2 ==  0){n /= 2;}
-8            else if(n % 3 == 0){n /= 3;}
-9            else if(n %5 == 0){n /= 5;}
-10        }
-11        return (n == 1) ? true: false;
-12    }
-13};
+class Solution {
+public:
+    bool isUgly(int n) 
+    {
+        if (n <= 0) return false;
+        while(n > 1)
+        {
+            if (n % 2 == 0) n = n / 2;
+            else if (n % 3 == 0) n = n / 3;
+            else if (n % 5 == 0) n = n / 5;
+            else break;
+        }
+        return (n == 1);
+    }
+};
