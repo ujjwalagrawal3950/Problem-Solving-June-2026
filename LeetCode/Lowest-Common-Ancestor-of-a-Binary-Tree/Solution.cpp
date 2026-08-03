4b28d22c686265d645194e43f7a0ab39dd1d827e
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-08-03 13:41:26 +0530

    Add solution for Lowest Common Ancestor of a Binary Tree

--- LeetCode/Lowest-Common-Ancestor-of-a-Binary-Tree/Solution.cpp
+++ LeetCode/Lowest-Common-Ancestor-of-a-Binary-Tree/Solution.cpp
@@ -8,21 +8,20 @@
 8 * };
 9 */
 10class Solution {
-11    TreeNode*LCA(TreeNode*root, int p , int q){
-12        if(root == nullptr || root->val == p || root->val == q){
-13            return root;
-14        }
+11    TreeNode * LCA(TreeNode*root, int p, int q){
+12        if(root == nullptr){return nullptr;}
+13        if(root->val == p || root->val == q){return root;}
+14
 15        TreeNode*left = LCA(root->left, p, q);
-16        TreeNode*right= LCA(root->right,p, q);
+16        TreeNode*right = LCA(root->right, p, q);
 17
-18        if(!left){return right;}
-19        else if(!right) {return left;}
-20        // if left and right both returning node then return root because root become diverging node;
-21        
-22        return root;
-23    }
-24public:
-25    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-26        return LCA(root, p->val, q->val); // we need to compare values only
-27    }
-28};
+18        if(left == nullptr){return right;}
+19        if(right == nullptr){return left;}
+20
+21        return root;
+22    }
+23public:
+24    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+25        return LCA(root, p->val, q->val);
+26    }
+27};
