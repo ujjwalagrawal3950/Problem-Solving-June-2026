af45dce3b27b92db6c238888dab1b5e780f86181
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-08-03 12:36:41 +0530

    Add solution for Matchsticks to Square

--- LeetCode/Matchsticks-to-Square/Solution.cpp
+++ LeetCode/Matchsticks-to-Square/Solution.cpp
@@ -1,33 +1,26 @@
 1class Solution {
-2    bool solution(vector<int>&matchsticks , int index , vector<int>&temp , int size){
-3        if(index < 0){
-4            for(int i = 0; i<3; i++){
-5                if(temp[i] != temp[i+1]){
-6                    return false;
-7                }
-8            }
-9            return true;
-10        }
-11
-12        for(int i = 0; i<4; i++){
-13            if(temp[i] + matchsticks[index] > size){
-14                continue;  // skip that element
-15            }
-16            if(i > 0 && temp[i] == temp[i-1]) continue; // optimization
-17            temp[i] += matchsticks[index];
-18            if(solution(matchsticks , index-1 , temp, size)) return true; // end tak aake agar square ban gaya toh bapash upar jane ki jarurat nahi hai islye return true kar do
-19            temp[i] -= matchsticks[index];
-20        }
-21        return false;
-22    }
-23public:
-24    bool makesquare(vector<int>& matchsticks) {
-25        int n = matchsticks.size();
-26        int sum = accumulate(matchsticks.begin() , matchsticks.end(), 0);
-27        if(sum %2 != 0){return false;}
-28        int stick_size = sum/4;
-29        sort(matchsticks.begin() , matchsticks.end());
-30        vector<int>temp(4 , 0);
-31        return solution(matchsticks , n-1, temp , stick_size);
-32    }
-33};
+2    bool solution(vector<int>&matchsticks, int idx, int target, vector<int>&side){
+3        int n = matchsticks.size();
+4        if(idx >= n){
+5            if(side[0] == side[1] && side[1] == side[2] && side[2] == side[3]) return true;
+6        }
+7
+8        for(int i = 0; i<4; i++){ // Each sticks have to option to add in any of four side
+9            if(side[i] + matchsticks[idx] > target){continue;} // ek side badi ho gayi toh add hi nahi karna
+10            if(i > 0 && side[i] == side[i-1]) continue;
+11            side[i] += matchsticks[idx];
+12            if(solution(matchsticks, idx+1, target, side)) return true;
+13            side[i] -= matchsticks[idx];
+14        }
+15        return false;
+16    }
+17public:
+18    bool makesquare(vector<int>& matchsticks) {
+19        int n = matchsticks.size();
+20        int sum = accumulate(matchsticks.begin(), matchsticks.end(),0);
+21        if(sum % 2 != 0){return false;}
+22        sort(matchsticks.begin(), matchsticks.end());
+23        vector<int>sides(4, 0);
+24        return solution(matchsticks, 0, sum/4, sides);
+25    }
+26};
