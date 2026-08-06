aa53bc36d448a6dc181e103f8774b9aaca107b29
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-08-06 18:33:53 +0530

    Add solution for Smallest Divisible Digit Product I

--- LeetCode/Smallest-Divisible-Digit-Product-I/Solution.cpp
+++ LeetCode/Smallest-Divisible-Digit-Product-I/Solution.cpp
@@ -1,20 +1,12 @@
-1class Solution {
-2public:
-3    int smallestNumber(int n, int t) {
-4        int x = n, ans = 0;
-5        n += 10;
-6        while(x <= n){
-7            int num = x, product = 1;
-8            while(num){
-9                product *= (num%10);
-10                num /= 10;
-11            }
-12            if(product % t == 0){
-13                ans = x;
-14                break;
-15            }
-16            x++;
-17        }
-18        return ans;
-19    }
-20};
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        auto [q, r] = div(n, 10);
+
+        int req = t / gcd(q + (10 - q) / 10, t);
+        int nxt = ((r + req - 1) / req) * req;
+        int x = nxt - (nxt - 10) * (nxt / 10);
+
+        return q * 10 + x;
+    }
+};
