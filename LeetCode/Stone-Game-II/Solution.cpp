19cf5a2121ec7327a60c52d102ce6c01e1d0b0f9
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-08-13 20:54:41 +0530

    Add solution for Stone Game II

--- LeetCode/Stone-Game-II/Solution.cpp
+++ LeetCode/Stone-Game-II/Solution.cpp
@@ -1,28 +1,34 @@
-1class Solution {
-2    int dp[2][101][101];
-3    int solve(vector<int>&piles, int idx, int m, int person){
-4        int n = piles.size();
-5        if(idx >= n){return 0;}
-6        if(dp[person][idx][m] != -1){return dp[person][idx][m];}
-7        int stones = (person == 1)?-1:INT_MAX;
-8        int take = 0, x = min(2*m, n-idx);
-9
-10        for(int i = 1; i<=x; i++){  // Alice turn to play
-11            take += piles[idx+i-1];
-12            if(person){
-13                stones = max(stones, take + solve(piles, idx+i, max(m, i), 0));
-14            }
-15            else{
-16                stones = min(stones, solve(piles, idx+i, max(m, i), 1));
-17            } 
-18        }
-19        return dp[person][idx][m] =  stones;
-20    }
-21public:
-22    int stoneGameII(vector<int>& piles) {
-23        memset(dp, -1 , sizeof(dp));
-24        int sum = accumulate(piles.begin(), piles.end(), 0);
-25        int alice_score = solve(piles, 0, 1, 1);
-26        return alice_score;
-27    }
-28};
+class Solution {
+public:
+    int n;
+    int dp[2][101][101];
+    int alice(int isBob, int i, int m, vector<int>& piles){
+        if (i == n) return 0;
+        if (dp[isBob][i][m]!=-1) return dp[isBob][i][m];
+        int stones =(isBob == 0)? 0: INT_MAX;
+        int sum = 0, xN= min(2*m, n-i);
+        for (int x = 1; x <= xN; x++) {
+            sum += piles[i+x-1];
+            int m2=max(m, x);
+            if (isBob) 
+                stones=min(stones, alice(0, i+x, m2, piles));
+            else 
+                stones=max(stones, sum+alice(1, i+x, m2, piles));                       
+        }
+        return dp[isBob][i][m]=stones;
+    }
+
+    int stoneGameII(vector<int>& piles) {
+        n = piles.size();
+        memset(dp, -1, sizeof(dp));
+        return alice(0, 0, 1,  piles);
+    }
+};
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
