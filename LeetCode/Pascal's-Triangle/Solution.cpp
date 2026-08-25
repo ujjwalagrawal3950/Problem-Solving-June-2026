f06111208cf74b954b271bb5b6e79c58fcaee3af
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-08-26 01:04:06 +0530

    Add solution for Pascal's Triangle

--- LeetCode/Pascal's-Triangle/Solution.cpp
+++ LeetCode/Pascal's-Triangle/Solution.cpp
@@ -2,22 +2,19 @@
 2public:
 3    vector<vector<int>> generate(int numRows) {
 4        vector<vector<int>>ans;
-5        for(int i = 0; i<numRows; i++){
-6            vector<int>temp(i+1, 0);
-7            int k = 0;
+5        int k = 0;
+6        for(int i = 0; i<numRows; i++){
+7            vector<int>temp;
 8            for(int j = 0; j<i+1; j++){
-9
-10                if(j == 0 || j==i){
-11                    temp[j] = 1;
-12                }
-13                else{
-14                    int size = ans[i-1].size();
-15                    temp[j] = ans[i-1][k] + ans[i-1][k+1];
-16                    k++;
-17                }
-18            }
-19            ans.push_back(temp);
-20        }
-21        return ans;
-22    }
-23};
+9                if(j == 0 || j == i){
+10                    temp.push_back(1);
+11                }
+12                else{
+13                    if(i > 0){int sum = ans[i-1][j] + ans[i-1][j-1]; temp.push_back(sum);}
+14                }
+15            }
+16            ans.push_back(temp);
+17        }
+18        return ans;
+19    }
+20};
