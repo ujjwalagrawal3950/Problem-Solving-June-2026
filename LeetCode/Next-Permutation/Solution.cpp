7bf3d5a868d3e39ad047ce3b7e2f1e2bed2ea5e7
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-08-26 11:14:17 +0530

    Add solution for Next Permutation

--- LeetCode/Next-Permutation/Solution.cpp
+++ LeetCode/Next-Permutation/Solution.cpp
@@ -1,25 +1,27 @@
 1class Solution {
 2public:
-3    void nextPermutation(vector<int>& arr) {
-4        int n = arr.size();
-5        int index  = -1;
+3    void nextPermutation(vector<int>& nums) {
+4        int n = nums.size();
+5        int idx = -1;
 6        for (int i = n - 2; i >= 0; i--) {
-7            if (arr[i] < arr[i + 1]) {
-8                index = i;
-9                break;
-10            }
-11        }
-12        // cout<<"Index  "<<index<<endl;
-13        if(index == -1){
-14            return reverse(arr.begin() , arr.end());
-15        }
-16       //  2 | 1 5 4 3 0 0  example and we stop after two due to dip 
-17       for(int i = n-1 ; i>index ; i--){
-18            if(arr[i] > arr[index]){
-19                swap(arr[i] , arr[index]);
-20                break;
-21            }
-22       }
-23       return reverse(arr.begin()+index+1 , arr.end());
-24    }
-25};
+7            if (nums[i] <
+8                nums[i + 1]) { // Means we have some element in previous so we
+9                               // can make next greater element
+10                idx = i;
+11                break;
+12            }
+13        }
+14
+15        if (idx == -1) {
+16            reverse(nums.begin(), nums.end());
+17        } else {
+18            for (int i = n - 1; i >= idx; i--) {
+19                if (nums[i] > nums[idx]) {
+20                    swap(nums[i], nums[idx]);
+21                    reverse(nums.begin() + idx + 1, nums.end());
+22                    break;
+23                }
+24            }
+25        }
+26    }
+27};
