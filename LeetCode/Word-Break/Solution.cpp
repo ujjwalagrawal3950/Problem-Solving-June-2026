326e85d6a9fa991b0b3fb98f7576ad8d39fabd8f
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-08-29 16:45:18 +0530

    Add solution for Word Break

--- LeetCode/Word-Break/Solution.cpp
+++ LeetCode/Word-Break/Solution.cpp
@@ -1,27 +1,22 @@
 1class Solution {
-2    bool solution(string&s, vector<string>&word, unordered_set<string>&mp, int st, vector<int>&dp){
-3        if(dp[st] != -1){return dp[st];}
-4        int n = s.length();
-5        string temp = "";
-6        for(int i = st; i<n; i++){
-7            temp += s[i];
-8            if(mp.find(temp) != mp.end()){
-9                if(i == n-1) return dp[st] = true;
-10                if(i != n-1 && solution(s, word, mp, i+1, dp)) return dp[st] = true;
-11            }
-12        }
-13        return dp[st] = false;
-14    }
-15public:
-16    bool wordBreak(string s, vector<string>& wordDict) {
-17
-18        // here gready approch will fail because if we found wrong substring in the map and if we take it then this will give wrong answer 
-19
-20        // example s = 'leetcode' wordDict = {'leet', 'lee', 'code'} then firstly you choose lee and then further no words will available and you will give wrong answer. therefore use dyanmic programing here instead of gready approch
-21        int n = s.length();
-22        vector<int>dp(n, -1);
-23        unordered_set<string>mp(wordDict.begin(), wordDict.end());
-24        return solution(s, wordDict, mp, 0, dp);
-25
-26    }
-27};
+2    int dp[1001];
+3    bool check(string & s, unordered_set<string>&st, int idx){
+4        if(idx == s.length()){return true;}
+5        if(idx > s.length()) return false;
+6        if(dp[idx] != -1){return dp[idx];}
+7        string temp = "";
+8        for(int i = idx; i<s.length(); i++){
+9            temp += s[i];
+10            if(st.find(temp) != st.end()){
+11                if(check(s, st, i + 1)) return dp[idx] = true;
+12            }
+13        }
+14        return dp[idx] = false;
+15    }
+16public:
+17    bool wordBreak(string s, vector<string>& wordDict) {
+18        memset(dp, -1, sizeof(dp));
+19        unordered_set<string>st(wordDict.begin(), wordDict.end());
+20        return check(s, st, 0);
+21    }
+22};
