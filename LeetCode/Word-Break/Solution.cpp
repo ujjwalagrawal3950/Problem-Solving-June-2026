2448fec8a97712300f8dbca1e0aaa0087dd2fb52
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-08-29 16:20:10 +0530

    Add solution for Word Break

--- LeetCode/Word-Break/Solution.cpp
+++ LeetCode/Word-Break/Solution.cpp
@@ -1,27 +1,19 @@
 1class Solution {
-2    bool solution(string&s, vector<string>&word, unordered_set<string>&mp, int st, vector<int>&dp){
-3        if(dp[st] != -1){return dp[st];}
+2public:
+3    bool wordBreak(string s, vector<string>& wordDict) {
 4        int n = s.length();
-5        string temp = "";
-6        for(int i = st; i<n; i++){
-7            temp += s[i];
-8            if(mp.find(temp) != mp.end()){
-9                if(i == n-1) return true;
-10                if(i != n-1 && solution(s, word, mp, i+1, dp)) return dp[st] = true;
-11            }
-12        }
-13        return dp[st] = false;
-14    }
-15public:
-16    bool wordBreak(string s, vector<string>& wordDict) {
-17
-18        // here gready approch will fail because if we found wrong substring in the map and if we take it then this will give wrong answer 
-19
-20        // example s = 'leetcode' wordDict = {'leet', 'lee', 'code'} then firstly you choose lee and then further no words will available and you will give wrong answer. therefore use dyanmic programing here instead of gready approch
-21        int n = s.length();
-22        vector<int>dp(n, -1);
-23        unordered_set<string>mp(wordDict.begin(), wordDict.end());
-24        return solution(s, wordDict, mp, 0, dp);
-25
-26    }
-27};
+5        vector<bool>dp(n+1, false); // i = 0 means empty substr so we choose dp.size = n
+6        dp[0] = true;
+7
+8        for(int i = 1; i<=n; i++){
+9            for(string x : wordDict){
+10                int start = i - x.length();
+11                if(start >= 0 && dp[start] && s.substr(start, x.length()) == x){
+12                    dp[i] = true;
+13                    break;
+14                }
+15            }
+16        }
+17        return dp[n];
+18    }
+19};
