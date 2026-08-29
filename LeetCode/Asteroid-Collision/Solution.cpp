654c95c3003a8aa382af646d468da8ee8304e764
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-08-29 13:44:32 +0530

    Add solution for Asteroid Collision

--- LeetCode/Asteroid-Collision/Solution.cpp
+++ LeetCode/Asteroid-Collision/Solution.cpp
@@ -1,30 +1,30 @@
 1class Solution {
 2public:
-3    vector<int> asteroidCollision(vector<int>& asteroids) {
-4        vector<int>ans;
-5        stack<int>st;
-6        for(int i = 0; i<asteroids.size(); i++){
-7            if(asteroids[i] < 0){
-8                while(!st.empty() && st.top() < abs(asteroids[i])){
+3    vector<int> asteroidCollision(vector<int>& nums) {
+4        stack<int>st; vector<int>ans;
+5        for(int i = 0 ; i<nums.size(); i++){
+6            bool alive = true;
+7            while(!st.empty() && alive && nums[st.top()] > 0  && nums[i] < 0){
+8                if(nums[st.top()] < -nums[i]){
 9                    st.pop();
 10                }
-11                if(st.empty()){
-12                   ans.push_back(asteroids[i]);
-13                }
-14                if(!st.empty() && (st.top() == abs(asteroids[i]))){
-15                    st.pop();
-16                }
-17            }
-18            else{
-19                st.push(asteroids[i]);
+11                else if(nums[st.top()] == -nums[i]){
+12                    st.pop();
+13                    alive = false;
+14                }
+15                else{  
+16                    // means +ve direction asteriod are bigger than -ve direction and they both collide and +ve direction is going on
+17
+18                    alive = false; // this is restrict to add -ve element because here bigger +ve direction planet is win
+19                }
 20            }
-21        }
-22        int size = ans.size();
+21            if(alive) st.push(i);
+22        }
 23        while(!st.empty()){
-24            ans.push_back(st.top());
+24            ans.push_back(nums[st.top()]);
 25            st.pop();
 26        }
-27        reverse(ans.begin()+size, ans.end());
+27        reverse(ans.begin(), ans.end());
 28        return ans;
 29    }
 30};
