3e3beff0872d9fbc2e3e5d9b20c8dd4360486b00
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-08-31 11:48:25 +0530

    Add solution for Customers Who Never Order

--- LeetCode/Customers-Who-Never-Order/Solution.sql
+++ LeetCode/Customers-Who-Never-Order/Solution.sql
@@ -1,1 +1,5 @@
-return df['Customers'].to_frame()
+1# Write your MySQL query statement below
+2Select c.name as Customers From Customers c
+3Left Join Orders o
+4    On c.id = o.customerId
+5    Where o.id IS NULL
