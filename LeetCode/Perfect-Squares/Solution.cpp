2bed941791c22260fd82733cc177f279f35498ec
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-09-04 17:51:09 +0530

    Add solution for Perfect Squares

---
+++ LeetCode/Perfect-Squares/Solution.cpp
@@ -0,0 +1,8 @@
+ Lagrange's Four Square Theorem states that every natural number can be represented as the sum of four integer squares.
+
+Based on this theorem, there are only four possible results for any given integer n:
+
+If n itself is a perfect square, then the result is 1.
+If n can be expressed as the sum of two perfect squares, then the result is 2.
+If n can be expressed as the sum of three perfect squares, then the result is 3.
+If none of the above cases apply, then the result is 4.
