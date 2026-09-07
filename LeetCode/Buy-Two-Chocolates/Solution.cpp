0c2112dd20114f77b32e4ec99b3b6c407ea729d1
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-09-07 23:57:34 +0530

    Add solution for Buy Two Chocolates

--- LeetCode/Buy-Two-Chocolates/Solution.cpp
+++ LeetCode/Buy-Two-Chocolates/Solution.cpp
@@ -1,7 +1,1 @@
-1class Solution {
-2public:
-3    int buyChoco(vector<int>& prices, int money) {
-4        sort(prices.begin(), prices.end());
-5        return (prices[0] + prices[1] <= money) ? money - prices[0] - prices[1] : money;
-6    }
-7};
+second_min_cost = min(second_min_cost, current_cost)
