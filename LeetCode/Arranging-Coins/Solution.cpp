b2066a20ed2863f041a7c43fd0110acba874ca6f
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-09-13 23:38:15 +0530

    Add solution for Arranging Coins

--- LeetCode/Arranging-Coins/Solution.cpp
+++ LeetCode/Arranging-Coins/Solution.cpp
@@ -1,10 +1,6 @@
-1class Solution {
-2public:
-3    int arrangeCoins(int n) {
-4        int k = 1;
-5        while(1LL*k * (k + 1) / 2 <= n){
-6            k++;
-7        }
-8        return k-1;
-9    }
-10};
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        return (sqrt(1 + 8LL  *n) - 1 )/2;
+    }
+};
