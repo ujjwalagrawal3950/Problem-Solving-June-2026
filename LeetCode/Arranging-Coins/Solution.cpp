34ef51866d220092aad56bb035f74a22e493e377
ujjwalagrawal3950 <155886524+ujjwalagrawal3950@users.noreply.github.com>
2026-09-13 23:42:08 +0530

    Add solution for Arranging Coins

--- LeetCode/Arranging-Coins/Solution.cpp
+++ LeetCode/Arranging-Coins/Solution.cpp
@@ -1,6 +1,15 @@
-class Solution {
-public:
-    int arrangeCoins(int n) {
-        return (sqrt(1 + 8LL  *n) - 1 )/2;
-    }
-};
+1class Solution {
+2public:
+3    int arrangeCoins(int n) {
+4        int left = 1, right = n;
+5        while(left <= right){
+6            int mid = left + (right-left)/2;
+7            
+8            long long coins = 1LL * mid * (mid + 1) / 2;
+9            if(coins == n) return mid;
+10            else if(coins < n) left = mid + 1;
+11            else right = mid -1;
+12        }
+13        return right;
+14    }
+15};
